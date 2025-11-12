43234075c3bd04df5bf6d4befc7b6542a2bcddf6
xcCodebreaker <xccodebreaker619@gmail.com>
2025-11-12 20:50:57 +0600

    Testing routing for pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom';
+import ErrorPage from './components/ErrorPage';
+import Home from './components/Home';
+import Movies from './components/Movies';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {index: true, element: <Home />},
+      {
+        path: "/movies",
+        element: <Movies />,
+      }
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
